9c698d014a1961737ae624d7082a51a82109a8a5
islammd39e <islammdrahat308@gmail.comm>
2022-09-16 17:18:23 +0600

    original

--- rev.js
+++ rev.js
@@ -1,1 +1,23 @@
-console.log('life is hard');
+const chalerDam = 28;
+const dalerDam = 33;
+
+
+function jogKor (first,second){
+    const jogFol =  first + second;
+    return jogFol;
+}
+const total = jogKor(chalerDam,dalerDam)
+// console.log(total);
+
+function duiGunKor(num){
+    const gunFol =  num * 2;
+    return gunFol;
+}
+
+function dharDay(dharAmount){
+    const frinderTaka = dharAmount;
+    return frinderTaka;
+}
+const dharLagbe = duiGunKor(total)
+const takaPaichi = dharDay(dharLagbe)
+console.log(takaPaichi)
