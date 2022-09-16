367a49cbc103ec465b009530ff0f4774810b3494
islammd39e <islammdrahat308@gmail.comm>
2022-09-16 17:40:58 +0600

    original upload

--- rev.js
+++ rev.js
@@ -2,22 +2,32 @@ const chalerDam = 28;
 const dalerDam = 33;
 
 
-function jogKor (first,second){
-    const jogFol =  first + second;
+function jogKor(first, second) {
+    const jogFol = first + second;
     return jogFol;
 }
-const total = jogKor(chalerDam,dalerDam)
+const total = jogKor(chalerDam, dalerDam)
 // console.log(total);
 
-function duiGunKor(num){
-    const gunFol =  num * 2;
+function duiGunKor(num) {
+    const gunFol = num * 2;
     return gunFol;
 }
 
-function dharDay(dharAmount){
+function dharDay(dharAmount) {
     const frinderTaka = dharAmount;
     return frinderTaka;
 }
+
 const dharLagbe = duiGunKor(total)
 const takaPaichi = dharDay(dharLagbe)
 console.log(takaPaichi)
+
+const parHours = (second) => {
+    const minutes = second / 60
+    const hours = minutes / 60
+    return hours;
+}
+const result = parHours(10800)
+const init = parseInt(result)
+// console.log(init);
